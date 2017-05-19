e447055917bc198abdc3c37166e30e054c1063d4
23Pstars <23Pstars@gmail.com>
2017-05-19 07:54:18 +0700

    update listening port through environment

--- app.js
+++ app.js
@@ -12,24 +12,26 @@ app.use('/assets/fonts', express.static(__dirname + '/node_modules/bootstrap/dis
 
 app.use('/tes.json', express.static(__dirname + '/tes.json'));
 
-app.get('/', function (req, res) {
-    res.render('index');
+app.get('/', function(req, res) {
+  res.render('index');
 });
 
-app.get('/fetch/:site', function (req, res) {
-    const options = {
-        method: 'GET',
-        uri: 'http://api.lrsoft.id/ibm-watson/v1/tone-analyzer',
-        qs: {
-            site: req.params.site,
-            limit: 50
-        }
-    };
-    request(options, function (error, response, body) {
-        res.end(body);
-    });
+app.get('/fetch/:site', function(req, res) {
+  const options = {
+    method: 'GET',
+    uri: 'http://api.lrsoft.id/ibm-watson/v1/tone-analyzer',
+    qs: {
+      site: req.params.site,
+      limit: 50
+    }
+  };
+  request(options, function(error, response, body) {
+    res.end(body);
+  });
 });
 
-app.listen(8080, function () {
-    console.log("Listening on port 8080");
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log("Listening on port " + port);
 });
